a8d4e87a1dc17821012fe8e5d204e5f8ea4443cb
Sanjida Nusrat Ananna <sn.ananna@yahoo.com>
2022-09-19 01:47:37 +0600

    Add files via upload

---
+++ pvsoenix.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/header/header'
+import React from 'react'
+import RandDelement from '../components/RandDElement/RandDelement'
+
+const RanddPage = () => {
+  return (
+    <>
+      <Header></Header>
+      <RandDelement></RandDelement>
+    </>
+  )
+}
+
+export default RanddPage
